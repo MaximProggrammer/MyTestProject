db17a40b74b9b42adaa88cb1b28d07537ebd23ae
maxim <maxrocers@gmail.com>
2018-02-03 16:14:50 +0600

    добавили левый файл в гитигноре

--- app/src/main/java/com/mysite/maxim/mytestproject/MainActivity.java
+++ app/src/main/java/com/mysite/maxim/mytestproject/MainActivity.java
@@ -39,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
         addOnClickListener();
     }
 
-
-
     public void addOnClickListener (){
         B[1].setOnClickListener(
                 new View.OnClickListener() {
