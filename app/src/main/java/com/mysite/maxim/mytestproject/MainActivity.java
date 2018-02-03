2c34d12b46c2d21e2f43c1799f0d8f7684c4ebc9
maxim <maxrocers@gmail.com>
2018-02-03 17:06:16 +0600

    Сделали 3 на 3 а надо 4 на 4

--- app/src/main/java/com/mysite/maxim/mytestproject/MainActivity.java
+++ app/src/main/java/com/mysite/maxim/mytestproject/MainActivity.java
@@ -7,63 +7,62 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean[] vis;
-
-    Button[] buttons;
+    Button[][] bb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        vis = new boolean[8];
-        vis[0] = false;
-        vis[1] = false;
-        vis[2] = false;
-        vis[3] = false;
-        vis[4] = false;
-        vis[5] = false;
-        vis[6] = false;
-        vis[7] = false;
-        vis[8] = false;
 
-        buttons = new Button[8];
+        bb = new Button[3][];
+        bb[0]=new Button[3];
+        bb[1]=new Button[3];
+        bb[2]=new Button[3];
 
-        buttons[0] = (Button)findViewById(R.id.One);
-        buttons[1] = (Button)findViewById(R.id.Two);
-        buttons[2] = (Button)findViewById(R.id.Three);
-        buttons[3] = (Button)findViewById(R.id.Four);
-        buttons[4] = (Button)findViewById(R.id.Five);
-        buttons[5] = (Button)findViewById(R.id.Six);
-        buttons[6] = (Button)findViewById(R.id.Seven);
-        buttons[7] = (Button)findViewById(R.id.Eight);
-        buttons[8] = (Button)findViewById(R.id.Nine);
+        bb[0][0] = findViewById(R.id.One);
+        bb[0][1] = findViewById(R.id.Two);
+        bb[0][2] = findViewById(R.id.Three);
+        bb[1][0] = findViewById(R.id.Four);
+        bb[1][1] = findViewById(R.id.Five);
+        bb[1][2] = findViewById(R.id.Six);
+        bb[2][0] = findViewById(R.id.Seven);
+        bb[2][1] = findViewById(R.id.Eight);
+        bb[2][2] = findViewById(R.id.Nine);
 
-        addOnClickListener();
+        for (int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+
+                final int ii=i, jj=j;
+
+                bb[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        clickBB(ii, jj);
+                    }
+                });
+
+            }
+        }
+
+        initStart();
+
     }
 
-    public void addOnClickListener (){
-        buttons[1].setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        buttons[2].setVisibility(View.VISIBLE);
-                        buttons[1].setVisibility(View.INVISIBLE);
-                        buttons[2].setText("2");
-                        vis[1] = true;
-                    }
-                }
-        );
-        buttons[2].setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        buttons[3].setVisibility(View.VISIBLE);
-                        buttons[2].setVisibility(View.INVISIBLE);
-                        buttons[3].setText("2");
-                        vis[2] = true;
-                    }
-                }
-        );
+    private void initStart() {
+        for (int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+
+                bb[i][j].setVisibility(View.VISIBLE);
+                bb[i][j].setText("Y");
+
+            }
+        }
     }
+
+    private void clickBB(int i, int j) {
+        System.out.println("i = "+i+", j = "+j);
+        bb[i][j].setVisibility(View.INVISIBLE);
+    }
+
 }
 
