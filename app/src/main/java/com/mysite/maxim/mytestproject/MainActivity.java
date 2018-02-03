dd29a195b10747fa6b9f34a050fdfd63e9418633
maxim <maxrocers@gmail.com>
2018-02-03 16:19:52 +0600

    переименовали B V на buttons vis

--- app/src/main/java/com/mysite/maxim/mytestproject/MainActivity.java
+++ app/src/main/java/com/mysite/maxim/mytestproject/MainActivity.java
@@ -7,48 +7,49 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean[] V;
-    Button[] B;
+    boolean[] vis;
+
+    Button[] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        V = new boolean[8];
-        V[0] = false;
-        V[1] = false;
-        V[2] = false;
-        V[3] = false;
-        V[4] = false;
-        V[5] = false;
-        V[6] = false;
-        V[7] = false;
-        V[8] = false;
+        vis = new boolean[8];
+        vis[0] = false;
+        vis[1] = false;
+        vis[2] = false;
+        vis[3] = false;
+        vis[4] = false;
+        vis[5] = false;
+        vis[6] = false;
+        vis[7] = false;
+        vis[8] = false;
 
-        B = new Button[8];
+        buttons = new Button[8];
 
-        B[0] = (Button)findViewById(R.id.One);
-        B[1] = (Button)findViewById(R.id.Two);
-        B[2] = (Button)findViewById(R.id.Three);
-        B[3] = (Button)findViewById(R.id.Four);
-        B[4] = (Button)findViewById(R.id.Five);
-        B[5] = (Button)findViewById(R.id.Six);
-        B[6] = (Button)findViewById(R.id.Seven);
-        B[7] = (Button)findViewById(R.id.Eight);
-        B[8] = (Button)findViewById(R.id.Nine);
+        buttons[0] = (Button)findViewById(R.id.One);
+        buttons[1] = (Button)findViewById(R.id.Two);
+        buttons[2] = (Button)findViewById(R.id.Three);
+        buttons[3] = (Button)findViewById(R.id.Four);
+        buttons[4] = (Button)findViewById(R.id.Five);
+        buttons[5] = (Button)findViewById(R.id.Six);
+        buttons[6] = (Button)findViewById(R.id.Seven);
+        buttons[7] = (Button)findViewById(R.id.Eight);
+        buttons[8] = (Button)findViewById(R.id.Nine);
 
         addOnClickListener();
     }
 
     public void addOnClickListener (){
-        B[1].setOnClickListener(
+        buttons[1].setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        B[2].setVisibility(View.VISIBLE);
-                        B[1].setVisibility(View.INVISIBLE);
-                        B[2].setText("2");
-                        V[1] = true;
+                        buttons[2].setVisibility(View.VISIBLE);
+                        buttons[1].setVisibility(View.INVISIBLE);
+                        buttons[2].setText("2");
+                        vis[1] = true;
                     }
                 }
         );
