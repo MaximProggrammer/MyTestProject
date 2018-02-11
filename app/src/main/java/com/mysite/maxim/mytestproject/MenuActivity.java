679e215b472cb4986a76321eb3c1ab4675808882
maxim <maxrocers@gmail.com>
2018-02-11 17:38:52 +0600

    Всё работает. Сделал меню. Делаем дизаин кнопок.

--- app/src/main/java/com/mysite/maxim/mytestproject/MenuActivity.java
+++ app/src/main/java/com/mysite/maxim/mytestproject/MenuActivity.java
@@ -1,8 +1,8 @@
 package com.mysite.maxim.mytestproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,17 +14,20 @@ public class MenuActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_menu);
-    play_Game = (Button)findViewById(R.id.Play);
-  }
-  public void addOnClickListener(){
+    play_Game = (Button) findViewById(R.id.Play);
     play_Game.setOnClickListener(
         new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-            Intent Play = new Intent("com.mysite.maxim.mytestproject.MainActivity");
-            startActivity(Play);
+            ClickMe();
           }
         }
     );
   }
+
+  public void ClickMe(){
+    Intent Play = new Intent("com.mysite.maxim.mytestproject.MainActivity");
+    startActivity(Play);
+  }
 }
+
