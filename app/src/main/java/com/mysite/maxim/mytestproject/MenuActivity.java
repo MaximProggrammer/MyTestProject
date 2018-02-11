45ebfe2ddfa8e7c6b2424745379e284495fd7564
maxim <maxrocers@gmail.com>
2018-02-11 16:50:36 +0600

    Всё готово

--- app/src/main/java/com/mysite/maxim/mytestproject/MenuActivity.java
+++ app/src/main/java/com/mysite/maxim/mytestproject/MenuActivity.java
@@ -1,13 +1,30 @@
 package com.mysite.maxim.mytestproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+  private Button play_Game;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_menu);
+    play_Game = (Button)findViewById(R.id.Play);
+  }
+  public void addOnClickListener(){
+    play_Game.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            Intent Play = new Intent("com.mysite.maxim.mytestproject.MainActivity");
+            startActivity(Play);
+          }
+        }
+    );
   }
 }
